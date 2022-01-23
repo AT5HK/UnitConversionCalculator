4928e52bc87110b1b49bde79eb2687289086e81d
Auston <auston.salvana@live.com>
2022-01-23 14:13:15 -0800

    Initial Commit

---
+++ UnitConversionCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConversionCalculator
+//
+//  Created by auston salvana on 1/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitConversionCalculator/UnitConversionCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionCalculatorApp.swift
+//  UnitConversionCalculator
+//
+//  Created by auston salvana on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
